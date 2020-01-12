b86ddc8d9ab2e888460e00166c358617deda976b
Lau Kar Rui <karrui.lau@gmail.com>
2020-01-12 21:53:30 +0800

    feat: create InklessIconButton widget

---
+++ lib/widgets/inkless_icon_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:goals_flutter/constants.dart';
+
+class InklessIconButton extends StatefulWidget {
+  final IconData icon;
+  final double size;
+
+  InklessIconButton({this.icon, this.size = 25});
+
+  @override
+  _InklessIconButtonState createState() => _InklessIconButtonState();
+}
+
+class _InklessIconButtonState extends State<InklessIconButton> {
+  bool _isTapDown = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Icon(
+        widget.icon,
+        size: widget.size,
+        color: _isTapDown ? Theme.of(context).primaryColor : Colors.black,
+      ),
+      onTap: () {
+        setState(() {
+          _isTapDown = false;
+        });
+        Navigator.pushNamed(context, settingsRoute);
+      },
+      onTapDown: (_) {
+        setState(() {
+          _isTapDown = true;
+        });
+      },
+      onTapCancel: () {
+        setState(() {
+          _isTapDown = false;
+        });
+      },
+    );
+  }
+}

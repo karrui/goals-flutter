af513f7f68b6c6448f5c890412a1128f05a97bdf
Lau Kar Rui <karrui.lau@gmail.com>
2020-01-12 22:52:39 +0800

    feat: clean up home screen UI
    New UI does not use Material AppBar and has a custom app bar instead

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,31 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-import '../../constants.dart';
 import '../../dummy_data.dart';
+import '../../widgets/inkless_icon_button.dart';
 import 'widgets/jar.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Goals'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(FontAwesomeIcons.cog),
-            onPressed: () => Navigator.pushNamed(context, settingsRoute),
-          )
-        ],
-      ),
-      body: Container(
-        color: Colors.grey[100],
-        child: ListView(
-          children: dummyJars
-              .map((jar) => Jar(
-                    jar: jar,
-                  ))
-              .toList(),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Current goals',
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      InklessIconButton(
+                        icon: FontAwesomeIcons.cog,
+                        size: 25.0,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: dummyJars.map((jar) => Jar(jar: jar)).toList(),
+              ),
+            ),
+          ],
         ),
       ),
     );

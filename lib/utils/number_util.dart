17d9eab1340106937dd71d6e44f20f402a334fce
Lau Kar Rui <karrui.lau@gmail.com>
2020-01-12 23:45:41 +0800

    feat: rename money util to number_util and add percentFormatter

--- lib/utils/number_util.dart
+++ lib/utils/number_util.dart
@@ -2,7 +2,12 @@ import 'package:intl/intl.dart';
 
 // TODO: Allow currency format to be set by user
 final currencyFormatter = new NumberFormat("#,##0.00", "en_SG");
+final percentFormatter = new NumberFormat.percentPattern("en_SG");
 
 String convertDoubleToCurrencyString(double value) {
   return currencyFormatter.format(value);
+}
+
+String convertDoubleToPercentString(double value) {
+  return percentFormatter.format(value);
 }

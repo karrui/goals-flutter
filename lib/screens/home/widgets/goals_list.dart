d623854d272d0b5666e31e062449fbddaf3a90c3
Lau Kar Rui <karrui.lau@gmail.com>
2020-01-16 20:48:42 +0800

    feat: add empty state for goals list

--- lib/screens/home/widgets/goals_list.dart
+++ lib/screens/home/widgets/goals_list.dart
@@ -10,27 +10,55 @@ class GoalsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<List<GoalModel>>(
-      builder: (ctx, goals, child) => ListView.builder(
-        itemCount: goals.length,
-        itemBuilder: (ctx, index) {
-          return GestureDetector(
-              child: Hero(
-                tag: goals[index].id,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 20.0, vertical: 16.0),
-                  child: GoalCard(
-                    goal: goals[index],
+      builder: (ctx, goals, child) => goals.isNotEmpty
+          ? ListView.builder(
+              itemCount: goals.length,
+              itemBuilder: (ctx, index) {
+                return GestureDetector(
+                    child: Hero(
+                      tag: goals[index].id,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20.0, vertical: 16.0),
+                        child: GoalCard(
+                          goal: goals[index],
+                        ),
+                      ),
+                    ),
+                    onTap: () {
+                      Provider.of<CurrentGoal>(context, listen: false).goal =
+                          goals[index];
+                      Navigator.pushNamed(context, goalDetailsRoute);
+                    });
+              },
+            )
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: FractionallySizedBox(
+                    widthFactor: 0.5,
+                    child: Opacity(
+                      opacity: 0.9,
+                      child: Image(
+                          image: AssetImage('assets/images/emptyGoals.png')),
+                    ),
                   ),
                 ),
-              ),
-              onTap: () {
-                Provider.of<CurrentGoal>(context, listen: false).goal =
-                    goals[index];
-                Navigator.pushNamed(context, goalDetailsRoute);
-              });
-        },
-      ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "No goals yet",
+                    style: Theme.of(context)
+                        .textTheme
+                        .body2
+                        .copyWith(letterSpacing: 0.15),
+                  ),
+                ),
+                Text("Add a goal and it will show up here.")
+              ],
+            ),
     );
   }
 }

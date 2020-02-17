81458bc188d1f185bfda99018d1986616235ffa2
Lau Kar Rui <karrui.lau@gmail.com>
2020-02-18 01:37:55 +0800

    fix: stream of goals list not updating

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../models/goal_model.dart';
+import '../services/database.dart';
 import 'auth/auth_screen.dart';
 import 'home/home_screen.dart';
 
@@ -15,7 +18,11 @@ class SplashScreen extends StatelessWidget {
           if (user == null) {
             return AuthScreen();
           }
-          return HomeScreen();
+          return StreamProvider<List<GoalModel>>(
+            initialData: [],
+            create: (_) => DatabaseService().streamGoals(user),
+            child: HomeScreen(),
+          );
         } else {
           return Scaffold(
             body: Center(
